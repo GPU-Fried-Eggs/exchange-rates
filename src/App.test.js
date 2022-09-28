ddb8cba717e70b39fecbfa38f04e4b0cfa934521
GPU-Fried-Eggs <GPU-Fried-Eggs@users.noreply.github.com>
2022-09-28 22:41:53 +0000

    deploy: 9781edec9eeee19c6b53bf8dd6a6e212723ea16f

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const fiexedElement = screen.getByText(/Current exchange Rate/i);
-  expect(fiexedElement).toBeInTheDocument();
-});
